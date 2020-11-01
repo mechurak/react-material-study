d3f3a2d29770e2880171fe398b923844b7ad031d
sshim <mechurak@gmail.com>
2020-11-02 07:59:00 +0900

    Add estimate button (24. Using Buttons)
    - https://material-ui.com/components/buttons/#button

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/styles';
-import logo from '../../assets/logo.svg';
-import { Tab, Tabs } from '@material-ui/core';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import logo from "../../assets/logo.svg";
+import { Button, Tab, Tabs } from "@material-ui/core";
 
 function ElevationScroll(props) {
   const { children } = props;
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0
+    threshold: 0,
   });
 
   return React.cloneElement(children, {
@@ -18,7 +18,7 @@ function ElevationScroll(props) {
   });
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: "3em",
@@ -29,15 +29,22 @@ const useStyles = makeStyles(theme => ({
   tabContainer: {
     marginLeft: "auto",
   },
-  tab: {    
+  tab: {
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: "25px",
-  }
-}))
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    height: "45px",
+  },
+}));
 
 export default function Header(props) {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <React.Fragment>
       <ElevationScroll>
@@ -51,10 +58,17 @@ export default function Header(props) {
               <Tab className={classes.tab} label="About Us" />
               <Tab className={classes.tab} label="Contact Us" />
             </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
       <div className={classes.toolbarMargin} />
     </React.Fragment>
-  )
+  );
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -23,5 +23,11 @@ export default createMuiTheme({
       fontWeight: 700,
       fontSzie: "1rem",
     },
+    estimate: {
+      fontFamily: "Pacifico",
+      fontSize: "1rem",
+      textTransform: "none",
+      color: "white",
+    },
   },
 });
