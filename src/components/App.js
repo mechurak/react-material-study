58263f2daf73b7d158a0702bf531a9a4518c4bbf
sshim <mechurak@gmail.com>
2020-12-01 18:54:31 +0900

    Add footer stub (50. Footer Setup)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./ui/Theme";
 import Header from "../components/ui/Header";
+import Footer from "./ui/Footer";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/" component={() => <div style={{height: "2000px"}}>Home</div>} />
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route
             exact
@@ -35,6 +36,7 @@ function App() {
           />
           <Route exact path="/estimate" component={() => <div>Estimate</div>} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
